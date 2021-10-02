1594f96869de2c5fb481d34f80184592247970da
Jordan O'Leary <jdoleary@gmail.com>
2021-10-02 14:29:48 -0400

    fix: box renders in world space

--- src/main.rs
+++ src/main.rs
@@ -42,38 +42,42 @@ fn ball_movement_system(
     mut cursor_moved_events: EventReader<CursorMoved>,
     mut ball_query: Query<(&Ball, &mut Transform)>,
     // need to get window dimensions
-    // wnds: Res<Windows>,
+    wnds: Res<Windows>,
     // query to get camera transform
     // q_camera: Query<&Transform, With<MainCamera>>,
 ) {
     // clamp the timestep to stop the ball from escaping when the game starts
     let delta_seconds = f32::min(0.2, time.delta_seconds());
 
-    for event in cursor_moved_events.iter() {
-        // println!("{:?}", event);
+    // for event in cursor_moved_events.iter() {
+    //     println!("{:?}", event);
+    //     if let Ok((ball, mut transform)) = ball_query.single_mut() {
+    //         transform.translation.x = event.position.x;
+    //         transform.translation.y = event.position.y;
+    //     }
+    // }
+
+    // get the primary window
+    let wnd = wnds.get_primary().unwrap();
+
+    // check if the cursor is in the primary window
+    if let Some(pos) = wnd.cursor_position() {
+        // get the size of the window
+        let size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+
+        // the default orthographic projection is in pixels from the center;
+        // just undo the translation
+        let p = pos - size / 2.0;
+
+        // // assuming there is exactly one main camera entity, so this is OK
+        // let camera_transform = q_camera.single().unwrap();
+
+        // // apply the camera transform
+        // let pos_wld = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0);
+        eprintln!("World coords: {:?}", p);
         if let Ok((ball, mut transform)) = ball_query.single_mut() {
-            transform.translation.x = event.position.x;
-            transform.translation.y = event.position.y;
+            transform.translation.x = p.x;
+            transform.translation.y = p.y;
         }
     }
-
-    // // get the primary window
-    // let wnd = wnds.get_primary().unwrap();
-
-    // // check if the cursor is in the primary window
-    // if let Some(pos) = wnd.cursor_position() {
-    //     // get the size of the window
-    //     let size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
-
-    //     // the default orthographic projection is in pixels from the center;
-    //     // just undo the translation
-    //     let p = pos - size / 2.0;
-
-    //     // assuming there is exactly one main camera entity, so this is OK
-    //     let camera_transform = q_camera.single().unwrap();
-
-    //     // apply the camera transform
-    //     let pos_wld = camera_transform.compute_matrix() * p.extend(0.0).extend(1.0);
-    //     eprintln!("World coords: {}/{}", pos_wld.x, pos_wld.y);
-    // }
 }
